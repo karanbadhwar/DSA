e1199bd5dc8cfa952c989f1b586309dea4744955
karanbadhwar <karanbadhwar07@gmail.com>
2024-03-30 19:57:10 -0400

    sorted matrix half

--- 09-Search/Binary-Search/src/SortedMatrix.java
+++ 09-Search/Binary-Search/src/SortedMatrix.java
@@ -2,4 +2,29 @@ public class SortedMatrix {
     public static void main(String[] args) {
 
     }
+
+    static int[] binarySearch(int[][] matrix, int target, int row, int cStart, int cEnd)
+    {
+        while(cStart <= cEnd)
+        {
+            int mid = cStart + (cEnd - cStart) /2;
+            if(matrix[row][mid] == target)
+            {
+                return new int[]{row, mid};
+            }
+
+            if(matrix[row][mid] < target)
+            {
+                cStart++;
+            } else{
+                cEnd--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    static int[] search(int[][] matrix, int target)
+    {
+
+    }
 }
