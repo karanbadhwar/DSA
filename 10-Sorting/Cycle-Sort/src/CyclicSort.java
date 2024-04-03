a6af53b6e3f9774bbdfedf510e4a94661724998d
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-03 19:38:12 -0400

    Cyclic Sort

---
+++ 10-Sorting/Cycle-Sort/src/CyclicSort.java
@@ -0,0 +1,16 @@
+public class CyclicSort {
+    public static void main(String[] args) {
+
+    }
+    static void cyclicSort(int[] arr)
+    {
+        int i = 0;
+        while(i < arr.length)
+        {
+            if(arr[i] == arr[i] + 1)
+            {
+                swap(arr, )
+            }
+        }
+    }
+}
---
+++ 10-Sorting/Cycle-Sort/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+            When Given nos from range 1 to N => use Cyclic Sort
+         */
+        System.out.println("Cyclic Sort");
+    }
+}
