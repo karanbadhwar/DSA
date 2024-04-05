5f49779df65fb61bc84126e33961ef520389a6e7
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-05 09:43:06 -0400

    Cyclic Sort

--- 10-Sorting/Cycle-Sort/src/CyclicSort.java
+++ 10-Sorting/Cycle-Sort/src/CyclicSort.java
@@ -1,16 +1,31 @@
+import java.util.Arrays;
+
 public class CyclicSort {
     public static void main(String[] args) {
-
+        int[] arr = {3,5,2,1,4};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
     static void cyclicSort(int[] arr)
     {
         int i = 0;
         while(i < arr.length)
         {
-            if(arr[i] == arr[i] + 1)
+            int correctIndex = arr[i] - 1;
+
+            if(i != correctIndex) // if(arr[i] != arr[correctIndex])
             {
-                swap(arr, )
+                swap(arr, i, correctIndex);
+            } else{
+                i++;
             }
         }
+    }
+
+    static void swap(int[] arr, int presentIndex, int correctIndex)
+    {
+        int temp = arr[presentIndex];
+        arr[presentIndex] = arr[correctIndex];
+        arr[correctIndex] = temp;
     }
 }
