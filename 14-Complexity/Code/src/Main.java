5edb0058d52a355f9af422a50e2d866d262231d5
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-11 14:33:04 -0400

    Time and Space Complexity

---
+++ 14-Complexity/Code/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Time and Space Complexity!");
+
+        /*
+               Time Complexity != Time Taken
+
+               Time Complexity is a function gives us the relationship about -
+               how time will grow as the input grows.
+
+               Suppose in Linear Complexity - y = k*x;
+               k is the slope that shows how will time change as input grows
+               Note:- Y is Time taken and X is Input/Size
+         */
+
+    }
+}
