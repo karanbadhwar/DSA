27fcb6ce27e8ff995b898708e4b110aabc1f2bca
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-18 13:58:03 -0400

    Bitwise and Number system

---
+++ 15-Bitwise-NumberSystems/Code/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+          Bit Manipulation:-
+            Operators:-
+                1- AND(&) - Both needs to be TRUE
+                ( NOTE:- When we AND 1 with any Number, Digits remain the same!!
+                EX:- 1 1 0 0 1 0 1 0 0
+                     1 1 1 1 1 1 1 1 1
+                    ------------------------
+                     1 1 0 0 1 0 1 0 0  )
+
+                2 - OR(|) - Anyone needs to be TRUE
+
+                3 - XOR (^) Exclusive OR (If and only if) - Only ONE should be TRUE i.e.
+                     returns true (1) if the number of true inputs is odd, and false (0) if it's even.
+                     (
+                        NOTE:- When we XOR any number(suppose - a) with 1 we get complement of Number(a) i.e. Opposite
+                     )
+         */
+    }
+}
