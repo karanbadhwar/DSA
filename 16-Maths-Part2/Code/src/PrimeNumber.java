edc2bd8a8b20be2b5cb01aae11e3e81e870e7c5f
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-23 15:56:13 -0400

    Optimized Prime Number solution with SeiveOfEratosthenes

--- 16-Maths-Part2/Code/src/PrimeNumber.java
+++ 16-Maths-Part2/Code/src/PrimeNumber.java
@@ -1,6 +1,9 @@
 public class PrimeNumber {
     public static void main(String[] args) {
-
+        int n = 20;
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i + " " + isPrime(i));
+        }
     }
     static boolean isPrime(int n)
     {
---
+++ 16-Maths-Part2/Code/src/SeiveOfEratosthenes.java
@@ -0,0 +1,26 @@
+public class SeiveOfEratosthenes  {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] primes = new boolean[n+1];
+        sieve(n,primes);
+    }
+
+    static void sieve(int n, boolean[] primes)
+    {
+        for (int i = 2; i*i <= n ; i++) {
+            if(!primes[i])
+            {
+                for (int multiple = i*2; multiple <= n ; multiple+=i) {
+                    primes[multiple] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if(!primes[i])
+            {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
