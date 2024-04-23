1dfa40511d048c95fc3cf40cbd1441978ff5b4d3
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-23 18:51:47 -0400

    Newton Raphson Method for SQRT

---
+++ 16-Maths-Part2/Code/src/NewtonRaphsonSQRT.java
@@ -0,0 +1,27 @@
+public class NewtonRaphsonSQRT {
+    // Complexity O(logN)f(N)
+
+    //Formula -> root = (x + N/x)/2 X-> sqrt we assumed
+    public static void main(String[] args) {
+        System.out.println(sqrt(11));
+    }
+
+    static double sqrt(int n)
+    {
+        double x = n;
+        double root;
+
+        while(true)
+        {
+            root = (x + n/x)/2;
+
+            if(Math.abs(root - x) < 0.1)
+            {
+                break;
+            }
+            x = root;
+        }
+
+        return root;
+    }
+}
