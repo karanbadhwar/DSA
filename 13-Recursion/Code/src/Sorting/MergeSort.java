b110a3b6866a6411a6b77a350bcf1670edc75b46
karanbadhwar <karanbadhwar07@gmail.com>
2024-04-30 18:09:51 -0400

    Merge Sort with Recursion

---
+++ 13-Recursion/Code/src/Sorting/MergeSort.java
@@ -0,0 +1,67 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        arr = mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] mergeSort(int[] arr)
+    {
+        //Base Condition
+        if(arr.length == 1)
+        {
+            return arr;
+        }
+
+        int mid = arr.length/2;
+
+        int[] left = mergeSort(Arrays.copyOfRange(arr,0 ,mid)); // End is Exclusive!!
+        int[] right = mergeSort(Arrays.copyOfRange(arr,mid, arr.length)); // End is Exclusive!!
+
+        return merge(left,right);
+
+    }
+
+    private static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < first.length && j < second.length)
+        {
+            if(first[i] < second[j])
+            {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        //Possibility of one of Array being not Complete
+        //Copy the remaining Elements
+        while(i < first.length)
+        {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+
+        while(j < second.length)
+        {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
+    }
+}
