2477eb82c38d6f73187b9b74eb45854d803d9d57
karanbadhwar <karanbadhwar07@gmail.com>
2024-05-14 12:44:12 -0400

    Maze Problems

--- 13-Recursion/Code/src/Backtracking/Maze.java
+++ 13-Recursion/Code/src/Backtracking/Maze.java
@@ -1,10 +1,13 @@
 package Backtracking;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(count(3,3));
+        path("",3,3);
     }
 
+    //Returning Count
     static int count(int row, int col)
     {
         //Base Condition
@@ -17,5 +20,41 @@ public class Maze {
         int right = count(row, col -1);
 
         return left + right;
+    }
+
+    //Printing the Full Paths
+    static void path(String pro, int row, int col)
+    {
+        if(row == 1 && col == 1)
+        {
+            System.out.println(pro);
+            return;
+        }
+        if(row > 1) {
+            path(pro + "D", row - 1, col);
+        }
+        if(col>1) {
+            path(pro + "R", row, col - 1);
+        }
+    }
+
+    //Returning the Full Paths
+    static ArrayList<String> pathRet(String pro, int row, int col)
+    {
+        if(row == 1 && col == 1)
+        {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(pro);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        if(row > 1) {
+            list.addAll(pathRet(pro + "D", row - 1, col));
+        }
+        if(col>1) {
+            list.addAll(pathRet(pro + "R", row, col - 1));
+        }
+        return list;
     }
 }
