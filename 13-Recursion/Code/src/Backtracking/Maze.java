e67fee4e453de3a0f6f9365b3a2e0a1f7ed9fb73
karanbadhwar <karanbadhwar07@gmail.com>
2024-05-14 09:31:19 -0400

    Maze Problems

---
+++ 13-Recursion/Code/src/Backtracking/Maze.java
@@ -0,0 +1,21 @@
+package Backtracking;
+
+public class Maze {
+    public static void main(String[] args) {
+        System.out.println(count(3,3));
+    }
+
+    static int count(int row, int col)
+    {
+        //Base Condition
+        if(row == 1 || col == 1)
+        {
+            return 1;
+        }
+
+        int left = count(row -1, col);
+        int right = count(row, col -1);
+
+        return left + right;
+    }
+}
