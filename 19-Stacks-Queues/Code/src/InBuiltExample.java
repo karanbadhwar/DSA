dfbde076205b1808c6bf2af4a7c6bb3c51b7a3e0
karanbadhwar <karanbadhwar07@gmail.com>
2024-06-14 13:08:09 -0400

    Stacks n Queues

---
+++ 19-Stacks-Queues/Code/src/InBuiltExample.java
@@ -0,0 +1,39 @@
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class InBuiltExample {
+    public static void main(String[] args) {
+        /* Stack*/
+//        Stack<Integer> stack = new Stack();
+//
+//        stack.push(20);
+//        stack.push(45);
+//        stack.push(35);
+//        stack.push(19);
+//        stack.push(56);
+
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+
+    /* Queues*/
+//        Queue<Integer> queue = new LinkedList();
+//
+//        queue.add(3);
+//        queue.add(6);
+//        queue.add(9);
+//        queue.add(12);
+//        queue.add(15);
+
+//        System.out.println(queue.remove()); // 3 is removed.
+//        System.out.println(queue.peek()); // returns the head
+
+        /* Deque*/
+        Deque<Integer> deque = new LinkedList<>();
+    }
+}
---
+++ 19-Stacks-Queues/Code/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Stacks n Queues");
+        /*
+            Internally, these are also arrays.
+
+            Stacks - Same when we use Recursion, functions calls keep getting pilled up on stack.
+                    * In Stacks, what Comes in First is the last one to be removed.
+                    * LIFO -> Last In First Out || FILO -> First In Last Out.
+                    * Pop -> Removing an Item to the Stack && Push -> Adding an Item to the Stack
+
+            Queue - Take it as an Example, as Line (People standing in Line).
+                    * In Queues, what comes First is the First one to be removed.
+                    * FIFO -> First In First Out || LILO -> Last In Last Out.
+
+            Deque (Deck) - Doubly Ended Queue, can insert/Remove from both the sides.
+
+         */
+    }
+}
