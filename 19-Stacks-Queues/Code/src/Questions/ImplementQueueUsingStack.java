3295c432c97f7acde29f00ebff458d375f50092c
karanbadhwar <karanbadhwar07@gmail.com>
2024-06-18 22:10:49 -0400

    Implementation Queue Using Two Stacks Question

---
+++ 19-Stacks-Queues/Code/src/Questions/ImplementQueueUsingStack.java
@@ -0,0 +1,53 @@
+package Questions;
+
+//https://leetcode.com/problems/implement-queue-using-stacks
+
+import java.util.Stack;
+
+public class ImplementQueueUsingStack {
+    private Stack<Integer> main;
+    private Stack<Integer> helper;
+
+    public ImplementQueueUsingStack() {
+        main = new Stack<>();
+        helper = new Stack<>();
+    }
+
+
+    public void push(int x) {
+
+        //Linear Time Complexity for Insertion
+        /*
+        helper.push(x);
+        for (int i = 0; i < main.size(); i++) {
+            main.push(helper.pop());
+        }
+         */
+
+        main.push(x);
+    }
+
+    public int pop() {
+        while(!main.isEmpty())
+        {
+            helper.push(main.pop());
+        }
+
+        int removed = helper.pop();
+
+        while(!helper.isEmpty())
+        {
+            main.push(helper.pop());
+        }
+        return removed;
+    }
+
+    public int peek() {
+        return main.getFirst();
+    }
+
+    public boolean empty() {
+        return main.isEmpty();
+    }
+
+}
