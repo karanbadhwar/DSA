21fe23b5864604e604afaa5052802f8199092033
karanbadhwar <karanbadhwar07@gmail.com>
2024-06-25 11:11:19 -0400

    Nearest greater Element || Next Larges to Right

--- 19-Stacks-Queues/Code/src/Questions/GameOfTwoStacks.java
+++ 19-Stacks-Queues/Code/src/Questions/GameOfTwoStacks.java
@@ -3,40 +3,42 @@ package Questions;
 //https://www.hackerrank.com/challenges/game-of-two-stacks/problem
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
+
 
 public class GameOfTwoStacks {
 
     public static int twoStacks( int maxSum, List<Integer> a, List<Integer> b) {
-        return twoStacks(0,maxSum,a,b) - 1;
+        return twoStacks(0,maxSum,0,0, a, b) - 1;
     }
 
-    private static int twoStacks(int count, int maxSum, List<Integer>a, List<Integer>b)
+    private static int twoStacks(int count, int maxSum, int indexA, int indexB,List<Integer> a, List<Integer> b)
     {
         //Base Condition
         if(maxSum < 0)
         {
             return count;
         }
-        if(a.isEmpty() || b.isEmpty())
+        if(indexA == a.size() || indexB == b.size())
         {
             return count;
         }
 
 
-        int removedA = a.remove(0);
+        int removedA = a.get(indexA);
 
-        int removedB = b.remove(0);
+        int removedB = b.get(indexB);
 
-        int left = twoStacks( count+1,maxSum - removedA, a,b);
-        int right = twoStacks( count+1,maxSum - removedB, a, b);
+
+
+        int left = twoStacks( count+1,maxSum - removedA, indexA+1,indexB,a,b);
+        int right = twoStacks( count+1,maxSum - removedB, indexA, indexB + 1,a,b);
 
 
         return Math.max(left,right);
 
     }
+
 
     public static void main(String[] args) {
        ArrayList<Integer> a = new ArrayList<>();
@@ -46,10 +48,12 @@ public class GameOfTwoStacks {
        a.add(6);
        a.add(1);
        ArrayList<Integer> b = new ArrayList<>();
-        b.add(2);
+        b.add(2); 
         b.add(1);
         b.add(8);
         b.add(5);
-        System.out.println(twoStacks(0,10,a,b));
+//        System.out.println(twoStacks(0,10,a,b));
     }
+
+
 }
---
+++ 19-Stacks-Queues/Code/src/Questions/NLE_NGR.java
@@ -0,0 +1,61 @@
+package Questions;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class NLE_NGR {
+
+    /*
+        3 conditions:
+         1 -> Stack is empty -> return -1;
+         2 -> Stack.top() > arr[i] -> return stack.peek();
+         3 -> stack.top()< arr[i]
+                1 -> stack.pop()
+                2 -> stack.top > arr[i] -> return stack.peek()
+     */
+    public static ArrayList<Integer> NGR(int[] arr)
+    {
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            //Condition 1
+            if(stack.empty())
+            {
+                result.add(-1);
+                stack.push(arr[i]);
+            }
+
+            //Condition 2
+            else if(!stack.isEmpty() && stack.peek() > arr[i])
+            {
+                result.add(stack.peek());
+                stack.push(arr[i]);
+            }
+
+            //Condition 3
+            else if(!stack.isEmpty() && stack.peek() <= arr[i])
+            {
+                while(!stack.isEmpty() && stack.peek() <= arr[i])
+                {
+                    stack.pop();
+                }
+                result.add(stack.peek());
+                stack.push(arr[i]);
+            }
+        }
+        ArrayList<Integer> finalResult = new ArrayList<>();
+        for (int i = result.size()-1; i >= 0; i--) {
+            finalResult.add(result.get(i));
+        }
+
+        return finalResult;
+    }
+    public static void main(String[] args) {
+        //Concepts of Stack
+        // NGR -> Nearest Greatest to Right aka NLE -> Next Largest Element
+
+        int[] arr = {1,3,2,4};
+        System.out.println(NGR(arr));
+    }
+}
