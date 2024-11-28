3425ff1f38d9b29f12e649acf5534de7b3196677
karanbadhwar <karanbadhwar07@gmail.com>
2024-11-27 21:45:53 -0500

    BT max path sum

---
+++ 20-Trees/code/src/questions/BtMaxPathSum.java
@@ -0,0 +1,44 @@
+import javax.swing.tree.TreeNode;
+
+public class BtMaxPathSum {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        return helper(root);
+    }
+
+    public int helper(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftTree = helper(root.left);
+        int rightTree = helper(root.right);
+
+        leftTree = Math.max(0, leftTree);
+        rightTree = Math.max(0, rightTree);
+        int pathSUm = leftTree + rightTree + root.val;
+
+        max = Math.max(pathSUm, max);
+
+        return Math.max(leftTree,rightTree) + root.val;
+    }
+}
