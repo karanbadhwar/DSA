1b5a89e4f8042d8e3fbec8e681bf9c29013c709d
karanbadhwar <karanbadhwar07@gmail.com>
2025-07-01 20:30:33 -0400

    Josephus Problem

---
+++ 13-Recursion/code/src/JosephusProblem.java
@@ -0,0 +1,36 @@
+//https://www.geeksforgeeks.org/problems/game-of-death-in-a-circle1840/1
+
+import org.w3c.dom.ranges.Range;
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+public class JosephusProblem {
+    public static void main(String[] args) {
+        safePos(40, 7);
+    }
+
+    public static int safePos(int n, int k) {
+        if (n <= 1){
+            return 1;
+        }
+        ArrayList<Integer> people =
+                new ArrayList<>(IntStream.rangeClosed(1,n).boxed().toList());
+        solve(people, k-1, 0);
+        return people.get(0);
+    }
+
+    public static void solve(ArrayList<Integer> arr, int k, int currentIndex){
+        if (arr.size() == 1){
+            System.out.println(arr);
+            return;
+        }
+
+        // Removed Person
+        int removedIndex = (currentIndex + k) % arr.size();
+        arr.remove(removedIndex);
+
+        // Hypothesis
+        solve(arr, k, removedIndex);
+    }
+}
